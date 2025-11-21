149666a068c534dee20a1088952b35e15ab22152
Raja Muhammad Taha <rajataha151@gmail.com>
2025-11-21 15:17:35 +0500

    Implement palindrome checking functions

---
+++ q2.c
@@ -0,0 +1,24 @@
+#include <string.h>
+
+int isPalindrome(char* s) {
+    int left = 0;
+    int right = strlen(s) - 1;
+
+    while (left < right) {
+        if (s[left] != s[right]) {
+            return 0;  
+        }
+        left++;
+        right--;
+    }
+    return 1;  
+}
+
+char* firstPalindrome(char** words, int wordsSize) {
+    for (int i = 0; i < wordsSize; i++) {
+        if (isPalindrome(words[i])) {
+            return words[i];  
+        }
+    }
+    return "";  
+}
